e9591d4b377c48b1613406f0676f15091308e47e
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2022-10-24 16:38:12 +0800

    styling the input

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -13,7 +13,18 @@ function Input({ label, textInputConfig }) {
 export default Input;
 
 const styles = StyleSheet.create({
-  inputContainer: {},
-  label: {},
-  input: {},
+  inputContainer: {
+    backgroundColor: GlobalColor.colors.neutral100,
+    padding: 10,
+    marginBottom: 5,
+    borderRadius: 10,
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 6,
+  },
+  input: {
+    paddingVertical: 3,
+    fontSize: 16,
+  },
 });
