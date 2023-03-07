e385e0f391e52d2aabe31731b0cd92acc1b8204c
Jovan Lanutan <jolanutan@gmail.com>
2023-03-07 21:23:24 +0800

    working with chart

--- screens/Statistics.js
+++ screens/Statistics.js
@@ -1,12 +1,72 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Dimensions } from "react-native";
 import { SafeArea, SafeAreaInnerWrapper } from "../util/safe-area.component";
+import { PieChart } from 'react-native-chart-kit'
+
+const data = [
+  {
+    name: "Seoul",
+    population: 21500000,
+    color: "rgba(131, 167, 234, 1)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Toronto",
+    population: 2800000,
+    color: "#F00",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Beijing",
+    population: 527612,
+    color: "red",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "New York",
+    population: 8538000,
+    color: "#ffffff",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Moscow",
+    population: 11920000,
+    color: "rgb(0, 0, 255)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  }
+];
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
 
 const Statistics = () => {
   return (
     <SafeArea>
       <SafeAreaInnerWrapper>
-        <Text>Stats</Text>
+      <PieChart
+          data={data}
+          width={Dimensions.get('window').width}
+          height={220}
+          chartConfig={chartConfig}
+          accessor={"population"}
+          backgroundColor={"transparent"}
+          paddingLeft={"15"}
+          center={[10, 50]}
+          absolute
+      />
       </SafeAreaInnerWrapper>
     </SafeArea>
   );
